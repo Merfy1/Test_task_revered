df3c13a28cdb57839d407d4d55f098dabdca201f
Merfy1 <alexstor2003@gmail.com>
2024-01-30 20:31:11 +0300

    create: animation view cards

--- js/script.js
+++ js/script.js
@@ -83,3 +83,11 @@ cardsWrapper.addEventListener('click', function({ target }) {
       }
   }
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+      setTimeout(() => {
+          card.classList.add('visible');
+      }, index * 200); // Измените задержку по вашему усмотрению
+  });
+});
