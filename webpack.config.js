c7c17eba8972039cecce90a9912937467d81421d
Merfy1 <alexstor2003@gmail.com>
2024-01-31 00:17:15 +0300

    delete webpack

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './js/script.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-};
