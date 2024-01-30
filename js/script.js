1cea317d3d44faca04648d1cba244fdbc9400967
Merfy1 <alexstor2003@gmail.com>
2024-01-30 03:11:55 +0300

    change months

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-function changeCurrency(card) {
+function toggleBillingPeriod(timeElement) {
+    const cards = document.querySelectorAll('.card-price');
+    const isMonthly = timeElement.textContent.includes('Months');
+  
+    cards.forEach(card => {
+      const priceElement = card.querySelector('.price');
+      const perMonth = JSON.parse(priceElement.dataset.perMonth);
+  
+      if (isMonthly) {
+        priceElement.textContent = calculateDailyPrice(priceElement.textContent);
+        timeElement.textContent = '/Days';
+        priceElement.dataset.perMonth = 'false';
+      } else {
+        priceElement.textContent = getDefaultPrice(card);
+        timeElement.textContent = '/Months';
+        priceElement.dataset.perMonth = 'true';
+      }
+    });
+  }
+  
+  function calculateDailyPrice(monthlyPrice) {
+    return Math.round(parseFloat(monthlyPrice) / 30);
+  }
+  
+  function changeCurrency(card) {
     const priceElement = card.querySelector('.price');
     const currencySymbol = card.querySelector('.symbol');
   
@@ -10,13 +34,21 @@ function changeCurrency(card) {
       priceElement.textContent = convertToEuros(priceElement.dataset.defaultPrice);
     } else {
       currencySymbol.textContent = '$';
-      priceElement.textContent = priceElement.dataset.defaultPrice;
+      if (JSON.parse(priceElement.dataset.perMonth)) {
+        priceElement.textContent = getDefaultPrice(card);
+      } else {
+        priceElement.textContent = calculateDailyPrice(priceElement.dataset.defaultPrice);
+      }
     }
   }
   
   function changeCurrencySymbol(symbolElement) {
     const card = symbolElement.closest('.card-price');
     changeCurrency(card);
+  }
+  
+  function getDefaultPrice(card) {
+    return parseFloat(card.querySelector('.price').getAttribute('data-default-price'));
   }
   
   function convertToRubles(priceInDollars) {
